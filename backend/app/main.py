0200627bc36faaf5eb8b89a8f52d3f11a051c4c2
NikolayS93 <nikolays93@ya.ru>
2023-08-16 02:20:02 +0400

    Up

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
 
+from app.core import config
+from app.core.celery_app import celery_app
+from app import tasks
+
+
 app = FastAPI(
     docs_url="/api/docs",
     openapi_url="/api",
@@ -9,6 +14,13 @@ app = FastAPI(
 @app.get("/api/v1")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/api/v1/task")
+async def example_task():
+    celery_app.send_task("app.tasks.example_task", args=["Hello World"])
+
+    return {"message": "success"}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8888)
